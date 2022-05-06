ad396fc19e706711c3a35d97aa3ff2ae044df58e
Davy de Matos <davymatos01@gmail.com>
2022-05-06 20:40:51 -0300

    Função de upload de imagens
    Função de upload de imagens

--- Library/library/urls.py
+++ Library/library/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework import routers
 from books.api import viewsets
 
@@ -26,4 +28,4 @@ route.register(r'books', viewsets.BooksViewSet, basename="Books")
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(route.urls)),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
