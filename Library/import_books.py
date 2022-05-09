16558bbc3e6c32b2f93d4234e5544c1fe0ca3b8f
Davy de Matos <davymatos01@gmail.com>
2022-05-09 10:38:45 -0300

    Importando CSV
    Importando CSV

---
+++ Library/import_books.py
@@ -0,0 +1,31 @@
+import csv
+from library.books.models import models
+
+def csv_to_list(filename: str) -> list:
+    with open(filename) as csv_file:
+        reader = csv.DictReader(csv_file, delimiter=',')
+        csv_data = [line for line in reader]
+    return csv_data
+
+def save_data(data):
+    aux = []
+    for item in data:
+        title = str(item.get('title'))
+        author = str(item.get('author'))
+        release_year = item.get('release_year')
+        state = str(item.get('state'))
+        pages = item.get('pages')
+        publishing_company = str(item.get('publishing_company'))
+        obj = Books(
+            title = title,
+            author = author,
+            release_year = release_year,
+            state = state,
+            pages = pages,
+            publishing_company = publishing_company,
+        )
+        aux.append(obj)
+    Books.objects.bulk_create(aux)
+
+data = csv_to_list('fix/books.csv')
+save_data(data)
